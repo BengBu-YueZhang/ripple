d47939ba5f076828e5ef459dd723a510461c3a48
BengBu-YueZhang <zhangyue_coder@163.com>
2019-03-07 15:24:19 +0800

    更新代码

--- react-ripple-button/src/App.js
+++ react-ripple-button/src/App.js
@@ -2,14 +2,62 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import RippleButton from './components/RippleButton'
+import withRipple from './components/WithRipple'
+
+const WithBlock = withRipple(
+  (
+    <div className="content">
+      <h2>React</h2>
+      <h5>A JavaScript library for building user interfaces</h5>
+    </div>
+  )
+)
 
 class App extends Component {
   render() {
+    const codeFragment1 = `
+      <RippleButton>blank</RippleButton>
+
+      <RippleButton className="info">info</RippleButton>
+
+      <RippleButton className="error">error</RippleButton>
+
+      <RippleButton className="warning">warning</RippleButton>
+    `
+
+    const codeFragment2 = `
+      import withRipple from './components/WithRipple'
+
+      const WithBlock = withRipple(
+        (
+          <div className="content">
+            <h2>React</h2>
+            <h5>A JavaScript library for building user interfaces</h5>
+          </div>
+        )
+      )
+      
+      class Test extends React.Components {
+        render () {
+          return <WithBlock/>
+        }
+      }
+    `
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <h1>Ripple</h1>
+        <h2>RippleButton Component</h2>
+        <h4>How to use</h4>
+        <pre className="code">
+          <code>
+            {codeFragment1}
+          </code>
+        </pre>
+        <h4>Preview</h4>
         <div className="block">
           <RippleButton>blank</RippleButton>
         </div>
@@ -22,6 +70,15 @@ class App extends Component {
         <div className="block">
           <RippleButton className="warning">warning</RippleButton>
         </div>
+        <h2>WithRipple HOC</h2>
+        <h4>How to use</h4>
+        <pre className="code">
+          <code>
+            {codeFragment2}
+          </code>
+        </pre>
+        <h4>Preview</h4>
+        <WithBlock/>
       </div>
     )
   }
--- react-ripple-button/src/components/WithRipple/index.js
+++ react-ripple-button/src/components/WithRipple/index.js
@@ -1,3 +1,35 @@
-export default function withRipple (Component) {
+import React from 'react'
+import Ripple from '../Ripple'
+import './index.css'
 
-} 
+export default function withRipple (Component, rippleColor) {
+  const { children, className, onMouseDown } = Component.props
+
+  return class extends React.PureComponent {
+    constructor (props) {
+      super(props)
+      this.rippleRef = React.createRef()
+    }
+
+    handleMouseDown = (event) => {
+      onMouseDown && onMouseDown(event)
+      this.rippleRef.current.createRipple(event)
+    }
+
+    render () {
+      // 克隆原有JSX, 内部添加Ripple
+      return React.cloneElement(Component, {
+        className: `${className} with-ripple`,
+        onMouseDown: this.handleMouseDown,
+        children: (
+          <React.Fragment>
+            {
+              children
+            }
+            <Ripple rippleColor={rippleColor} ref={this.rippleRef}/>
+          </React.Fragment>
+        )
+      })
+    }
+  } 
+}
